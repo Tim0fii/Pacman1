1e812d597a068dca7a5ca8f864de2190249fcc56
Marta Mykhailyna <martamykhailyna1@gmail.com>
2025-05-04 16:31:08 +0300

    added func draw misc

--- main.py
+++ main.py
@@ -82,6 +82,25 @@ class Ghost:
         self.turns, self.in_box = self.check_collisions()
         self.rect = self.draw()
 
+def draw_misc():
+    score_text = font.render(f'Рахунок: {score}', True, 'white')
+    screen.blit(score_text, (10, 920))
+    if powerup:
+        pygame.draw.circle(screen, 'blue', (140, 930), 15)
+    for i in range(lives):
+        screen.blit(pygame.transform.scale(player_images[0], (30, 30)), (650 + i * 40, 915))
+    if game_over:
+        pygame.draw.rect(screen, 'white', [50, 200, 800, 300],0, 10)
+        pygame.draw.rect(screen, 'dark gray', [70, 220, 760, 260], 0, 10)
+        gameover_text = font.render('Гру завершено! Натисніть пробіл, щоб почати спочатку!', True, 'red')
+        screen.blit(gameover_text, (100, 300))
+    if game_won:
+        pygame.draw.rect(screen, 'white', [50, 200, 800, 300],0, 10)
+        pygame.draw.rect(screen, 'dark gray', [70, 220, 760, 260], 0, 10)
+        gameover_text = font.render('Перемога! Натисніть пробіл, щоб почати спочатку!', True, 'green')
+        screen.blit(gameover_text, (100, 300))
+
+
 def draw_board():
     num1 = ((HEIGHT - 50) // 32)
     num2 = (WIDTH // 30)
